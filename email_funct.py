66fb37b5d1e88e55bdc3871e20ab807f97c76672
nope2jope <67401076+nope2jope@users.noreply.github.com>
2021-04-27 22:12:59 -0700

    updated contact functionality
    contact page now employs POST and smtplib to notify a given user

---
+++ email_funct.py
@@ -0,0 +1,14 @@
+import smtplib
+
+def notify_user(email, pw, user_from, user_to, body_text):
+
+    try:
+        server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+        server.ehlo_or_helo_if_needed()
+        server.login(user=email, password=pw)
+        server.sendmail(from_addr=user_from, to_addrs=user_to, msg=body_text)
+        server.close()
+
+        print('email sent!')
+    except:
+        print('notify_user failed!')
--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
+from email_funct import notify_user
+import os
 
 
 application = Flask(import_name=__name__)
 
 all_posts = requests.get('https://api.npoint.io/e0eb025823d97820e87d').json()
+
+USER_EMAIL = os.environ['ENV_USER_EMAIL']
+USER_PASSWORD = os.environ['ENV_USER_PW']
+
 
 @application.route('/')
 def say_hi():
@@ -24,9 +30,20 @@ def fetch_post(num):
 def tender_about():
     return render_template('about.html')
 
-@application.route('/contact.html')
+@application.route('/contact', methods=['GET', 'POST'])
 def tender_contact():
-    return render_template('contact.html')
+    if request.method == 'POST':
+        credentials = request.form.items()
+        # reformat this body message so that fields are more elegantly represented
+        body_msg = ""
+        for item in credentials:
+            body_msg += item[1] + '\n'
+        # notification is being sent from and to same user for testing
+        notify_user(email=USER_EMAIL, pw=USER_PASSWORD, user_from=USER_EMAIL, user_to=USER_EMAIL, body_text=body_msg)
+        # return to confirmation page
+        return ("<h1>WOO</h1>")
+    elif request.method == 'GET':
+        return render_template('contact.html')
 
 
 application.run(debug=True)
